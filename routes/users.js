197c50ff395746787c831b839869472dc98a1534
Rishabh Verma <2000rishabhverma@gmail.com>
2023-09-29 19:36:14 +0530

    .

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,8 @@ const express =  require("express");
 const router = express.Router();
 
 // user routes
-router.get('/login', (req,res) => res.send('login'));
-router.get('/register', (req,res) => res.send('register'));
+router.get('/login', (req,res) => res.render('login'));
+router.get('/register', (req,res) => res.render('register'));
 
 
 module.exports = router;
